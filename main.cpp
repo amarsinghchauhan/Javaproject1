ff0d0f485ddcda4454da24fadb8e7dbfe1405e73
Esseddik ismael <ismael.esseddik@gmail.com>
2018-01-19 17:25:58 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "ABuGeR.h"
+#include "EAGL.h"
+
+
+int main()
+{
+    const char* file_path_Road ="1_data/paris_test/route_secondaire_buffer.shp";
+    const char* layer_name_Road ="route_secondaire_buffer";
+    const char* file_path_Parcel ="1_data/paris_test/test_paris_seuil.shp";
+    const char* layer_name_Parcel ="test_paris_seuil";
+    bool create_obj = true;
+    bool visualize = true;
+    int a(0);
+    int b(0);
+    if(create_obj)
+    {
+        int a = ABuGeR(file_path_Road, layer_name_Road, file_path_Parcel, layer_name_Parcel);
+    }
+    if (visualize)
+    {
+        int b = EAGL();
+    }
+    return a+b;
+}
